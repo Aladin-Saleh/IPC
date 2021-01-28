052140178820bfb6e04b37bd031bbed1711ee9f7
saleh <saleh@localhost>
2021-01-28 16:25:05 +0000

    ok

--- IPC/EXERCICE_DE_DENIS/src/ex1/incr.c
+++ IPC/EXERCICE_DE_DENIS/src/ex1/incr.c
@@ -6,64 +6,35 @@
 #include <sys/shm.h>
 #include <assert.h>
 #include <sys/types.h>
+
 #include <sys/wait.h>
 
 
-struct data
-{
-    int count;
-    char buf[256];
-
-
-};
 
 int main(int argc, char const *argv[])
 {
     key_t cle;
     int id;
-    struct data* s_data;
-    //char *buf;
-
+    int *count;
+    char * endPtr;
 
     cle = ftok("/tmp",1);
     assert(cle != -1);
 
-
-    id = shmget(cle,sizeof(struct data),0);
+    id = shmget(cle,sizeof(int),0);
     if (id == -1)
     {
         perror("Error id = -1...");
         exit(-1);
     }
 
-
-
-    s_data = (struct data*)shmat(id,NULL,SHM_R);
-    
-    
-    
-      printf("Avant incrémentation : %s\n",s_data->buf);
-      s_data->count++;
-//    printf("Apres incrémentation : %d\n----------\n",*count);
-
-    
-        
-
-
-  
-  if (shmctl(id,IPC_RMID,NULL) == -1)
-    {
-        perror("Error shmclt : lors de la suppression du segment mémoire");
-        exit(-1);
-        
-    }
+    count = (int*)shmat(id,NULL,0);
+    assert(count != (void*)-1);
+    *count = strtol(argv[1],&endPtr,10 )+ (*count)++;
+    printf("recu: %d\n----------\n",*count);
 
 
 
 
-
-
-
-    
     return 0;
 }
--- IPC/EXERCICE_DE_DENIS/src/ex1/main.c
+++ IPC/EXERCICE_DE_DENIS/src/ex1/main.c
@@ -7,48 +7,37 @@
 #include <assert.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-struct data
-{
-    int count;
-    char buf[256];
-
-};
 
 int main(int argc,char * argv[])
 {
-	int shmid,
-	    i;
-	    //*count;
-	struct data* s_data;
+	int shmid,i,*count;
+	char buf[256];
 
-	//char buf[256];
+
 	key_t k = ftok("/tmp",1);
 	assert(k!=-1);
 
-	shmid=shmget(k, sizeof(struct data), IPC_CREAT|0666);
+	shmid=shmget(k, sizeof(int), IPC_CREAT|0666);
 	assert(shmid >= 0);
 
-	s_data = (struct data*)shmat(shmid,NULL,0);
-	assert(s_data!= /*(void*)*/NULL);
+	count = (int*)shmat(shmid,NULL,0);
+	assert(count != (void*)-1);
 
-	s_data->count = 0;
+	*count = 0;
 
-	for (i=1;i<=5;i++){
+	for (i=1;i<=10;i++){
 		pid_t p = fork();
 		assert( p != -1);
 
 		if (p==0) {
-           
-			snprintf(s_data->buf,sizeof(s_data->buf),"%d",i);
-            execl("./incr","./incr",s_data->buf,NULL);
-            //printf("tu fous quoi ici ! \n");
-			exit(-1);
-            //assert(0);//n'est pas censé arrivé jusque la !
+			snprintf(buf,sizeof(buf),"%d",i);
+			execl("./incr","./incr",buf,NULL);
+			assert(0);//Pas censé arriver ici
 		}
 	}
 
-	for (i=1;i<=5;i++) wait(NULL);
+	for (i=1;i<=10;i++) wait(NULL);
 
-	printf("count = %d\n",s_data->count);
+	printf("count = %d\n",*count);
 	assert(shmctl(shmid,IPC_RMID,0) >=0);
 }
